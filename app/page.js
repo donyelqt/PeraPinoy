d5623a857f90c432462bf0afde63fdd77d9ddf35
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-26 17:34:52 +0800

    g"Updated font imports and usage in layout.js, added Button component to page.js"

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>PeraPinoy!</h2>
+      <Button
+      variant="outline"
+      >PeraGabay!</Button>
     </div>
   );
 }
