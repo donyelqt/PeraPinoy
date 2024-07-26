4e447121e729190935e33de841941e673fd6c4a0
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-26 18:46:30 +0800

    new size of logo

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,15 +1,17 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import React from 'react';
 
 function Header() {
   return (
-    <div className='p-4'>
+    <div className='p-8 flex justify-between items-center'>
       <Image
-        src={'/PeraPinoy.png'} // Note the leading slash
+        src={'/PeraPinoy1.png'} // Note the leading slash
         alt='logo'
-        width={160}
-        height={100}
+        width={95}
+        height={80}
       />
+      <Button>Get Started</Button>
     </div>
   );
 }
