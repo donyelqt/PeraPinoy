c084ae80bcb1c5e80d04aff7421b2d975c82d8ca
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-26 17:41:05 +0800

    Updated Roboto font configuration in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
-import { Inter, Open_Sans, Outfit } from "next/font/google";
+import { Inter, Open_Sans, Outfit, Roboto } from "next/font/google";
 import "./globals.css";
 
-const open_sans = Open_Sans({ subsets: ["latin"] });
+const roboto = Roboto({ 
+  subsets: ["latin"], 
+  weights: ["400"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={open_sans.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
