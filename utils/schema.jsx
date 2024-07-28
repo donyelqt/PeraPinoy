5d91d1c66e919cddab67dbdf2c43fec1d8635ade
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-28 19:06:57 +0800

    Added columns to Budgets table schema

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,5 +1,9 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const Budgets=pgTable('budgets', {
-    
+    id:serial('id').primaryKey(),
+    name:varchar('name').notNull(),
+    amount:varchar('amount').notNull(),
+    icon: varChar('icon'),
+    createdBy: varchar('createdBy')
 })
