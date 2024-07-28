91e519432e7534f1760f5236bc3e214210cdf345
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-28 16:00:26 +0800

    Updated SideNav component: added UserButton import, changed text color, and added styles to menu items and user button

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,3 +1,4 @@
+import { UserButton } from '@clerk/nextjs'
 import { LayoutDashboard, LayoutGrid, LucideShieldPlus, PiggyBankIcon, ReceiptIcon } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
@@ -26,20 +27,29 @@ function SideNav() {
     }
   ]
   return (
-    <div className='h-screen p-4 border shadow-md text-yellow-600'>
+    <div className='h-screen p-4 border shadow-md text-blue-600'>
       <Image src={'/PeraPinoy1.png'} 
       alt='logo'
       width={100}
       height={80}
       />
-      <div>
+      <div className='mt-5'>
         {menuList.map((menu,index)=>(
-          <h2>
+          <h2 className='flex gap-2 items-center
+          text-blue-600 font-medium 
+          p-4 cursor-pointer rounded-lg
+          hover:text-blue-600 hover:bg-blue-100
+          '>
             <menu.icon />
             {menu.name}
           </h2>
         ))}
       </div>
+          <div className='fixed bottom-10 p-5 flex gap-2
+          items-center'>
+            <UserButton />
+            Profile
+          </div>
     </div>
   )
 }
