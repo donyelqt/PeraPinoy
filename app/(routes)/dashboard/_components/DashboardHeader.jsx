edba0bfd0841487c49c4866743cc5fd5c318ab94
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-28 16:25:41 +0800

    Added UserButton component to DashboardHeader and modified layout, updated Dashboard page styling

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,10 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function DashboardHeader() {
   return (
-    <div className='p-5 shadow-md border-b text-gray-300'>
+    <div className='p-5 shadow-md border-b text-gray-300 flex justify-between'>
         <div>
-            
+            Search Bar
+        </div>
+        <div>
+            <UserButton />
         </div>
     </div>
   )
--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
+    <div className='text-gray-300'>
         Dashboard
     </div>
   )
