43855c66d1567e2fc15aee07e7d78a060854cb5b
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-07-29 20:13:52 +0800

    bring back the link of database

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+const sql = neon('postgresql://budget-planner_owner:m7sgJ3BUxNIF@ep-billowing-unit-a53qsyts.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require');
 export const db = drizzle(sql,{schema});
