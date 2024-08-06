c0e66609f64dd1569cb9727444f3cd5d10a5bdb1
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-07 00:43:45 +0800

    Added useRouter from next/navigation and modified DashboardLayout to redirect to /dashboard/budgets if user has no budgets

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -6,22 +6,24 @@ import { db } from '../../../utils/dbConfig';
 import { Budgets } from '../../../utils/schema';
 import { useUser } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
+import { useRouter } from 'next/navigation';
 
 function DashboardLayout({ children }) {
   const { user } = useUser();
-
+  const router = useRouter();
   useEffect(() => {
-    if (user) {
-      checkUserBudget();
-    }
+    user&&checkUserBudget();
   }, [user]);
 
   const checkUserBudget = async () => {
     const result = await db.select()
       .from(Budgets)
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
-
     console.log(result);
+    if(result?.length==0)
+    {
+      router.replace('/dashboard/budgets')
+    }
   }
 
   return (
