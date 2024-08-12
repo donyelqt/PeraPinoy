fd65dadf5bc6a2a933ec925f85c171870b0a95c7
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-12 14:49:38 +0800

    Added BudgetList component and updated JSX structure in Budget page

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -3,7 +3,7 @@ import BudgetList from './_components/BudgetList'
 
 function Budget() {
   return (
-    <div className='text-gray-300 p-10'>
+    <div className='text-blue-600 p-10'>
       <h2 className='text-bold text-3xl'>My Budgets</h2>
       <BudgetList />
     </div>
