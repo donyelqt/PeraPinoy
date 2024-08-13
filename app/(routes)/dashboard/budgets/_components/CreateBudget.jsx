c8fd0a0bb92d32f1fdfed9142cb5e1c80e3f1479
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-13 10:28:51 +0800

    add a dialog overlay

--- app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Dialog, DialogPortal } from "@radix-ui/react-dialog";
+import { Dialog, DialogOverlay, DialogPortal } from "@radix-ui/react-dialog";
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import { DialogContent } from "@radix-ui/react-dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
@@ -17,15 +17,17 @@ function CreateBudget() {
       </div>
       <Dialog>
         <DialogTrigger className="rounded p-1 mt-2 hover:bg-blue-500 hover:text-black bg-slate-300">Open</DialogTrigger>
-        <DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <DialogOverlay className="fixed inset-0 bg-black/50">
+        <DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md">
          <DialogPortal>
             <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
+            <DialogDescription className="fixed left-1/2 top-1/2 -translate-x-1/2 p-2 -translate-y-1/2 rounded-md text-gray-900 shadow bg-white">
               This action cannot be undone. This will permanently delete your account
               and remove your data from our servers.
             </DialogDescription>
           </DialogPortal>
         </DialogContent>
+        </DialogOverlay>
       </Dialog>
     </div>
   );
