c5b43d45f55de1ac059710a7d00397c68c847ea7
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-14 18:09:29 +0800

    Added 'mt-6' class to grid div element in ExpensesScreen component

--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -31,7 +31,7 @@ function ExpensesScreen({params}) {
   return (
     <div className='text-blue-600 p-10'>
         <h2 className='text-2xl font-bold'>My Expenses</h2>
-        <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div className='grid grid-cols-1 md:grid-cols-2 mt-6'>
             <BudgetItem 
             budget={budgetInfo}
             />
