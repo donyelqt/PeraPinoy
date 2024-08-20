ca77c394ba1f83c23c3d64c8ca4a0b3c85cf0ad5
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-20 16:57:32 +0800

    Added 'use client' directive and Clerk user data to Dashboard page

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,10 +1,12 @@
-import { UserButton } from '@clerk/nextjs'
+"use client"
+import { UserButton, useUser } from '@clerk/nextjs'
 import React from 'react'
 
 function Dashboard() {
+  const {user}=useUser();
   return (
-    <div className='text-gray-300'>
-        Dashboard
+    <div className='p-5'>
+        <h2 className='font-bold text-blue-600 text-3xl'>Hello, {user?.fullName}! 👋</h2>
     </div>
   )
 }
