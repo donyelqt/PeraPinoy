d9a9968fe21557c06b60a0ee7bdd0f65b99e0949
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-20 17:14:00 +0800

    Added state and effect to fetch budget list, imported additional dependencies, and rendered CardInfo component in Dashboard page

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,13 +1,42 @@
 "use client"
 import { UserButton, useUser } from '@clerk/nextjs'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CardInfo from "./_components/CardInfo"
+import { db } from '../../../utils/dbConfig';
+import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
+import { Budgets } from '../../../utils/schema';
 
 function Dashboard() {
   const {user}=useUser();
+
+  const [budgetList,setBudgetList]=useState([]);
+
+  useEffect(()=>{
+    user&&getBudgetList();
+  },[user])
+
+  // used to get budget list
+  const getBudgetList=async()=>{
+
+    const result=await db.select({
+       ...getTableColumns(Budgets),
+       totalSpend:sql `sum(${Expenses.amount}::numeric)`.mapWith(Number),
+       totalItem:sql `count(${Expenses.id})`.mapWith(Number)
+    }).from(Budgets)
+    .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
+    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
+    .groupBy(Budgets.id)
+    .orderBy(desc(Budgets.id))
+    ;
+
+    setBudgetList(result);
+
+  }
   return (
     <div className='p-10'>
         <h2 className='font-bold text-tertiary text-3xl'>Hello, {user?.fullName}! 👋</h2>
         <p className='text-gray-500'>Empower Your Finances with PeraPinoy</p>
+        <CardInfo />
     </div>
   )
 }
