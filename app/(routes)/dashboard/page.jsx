8aef25ed9f6c03e0c99a1e14f8d6a52920a29230
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-20 17:02:44 +0800

    Update Dashboard page: changed padding, text color, and added paragraph

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -5,8 +5,9 @@ import React from 'react'
 function Dashboard() {
   const {user}=useUser();
   return (
-    <div className='p-5'>
-        <h2 className='font-bold text-blue-600 text-3xl'>Hello, {user?.fullName}! 👋</h2>
+    <div className='p-10'>
+        <h2 className='font-bold text-tertiary text-3xl'>Hello, {user?.fullName}! 👋</h2>
+        <p className='text-gray-500'>Empower Your Finances with PeraPinoy</p>
     </div>
   )
 }
