d225c6a8f49c2af2516eb21dbf6996179b3df30b
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 18:26:07 +0800

    "Updated BarChartDashboard component: added ResponsiveContainer, changed h2 font size, and wrapped BarChart with div and ResponsiveContainer elements."

--- app/(routes)/dashboard/_components/BarChartDashboard.jsx
+++ app/(routes)/dashboard/_components/BarChartDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts'
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 function BarChartDashboard({ budgetList }) {
     return (
         <div className='border rounded-lg p-10'>
-            <h2 className='text-tertiary font-bold text-2xl'>Expense Breakdown</h2>
+            <h2 className='text-tertiary font-bold text-3xl'>Expense Breakdown</h2>
+            <div className="w-full h-64">
+            <ResponsiveContainer width="100%" height="100%">
             <BarChart
                 width={500}
                 height={300}
@@ -21,6 +23,8 @@ function BarChartDashboard({ budgetList }) {
                 <Bar dataKey="amount" stackId="a" fill='#87cefa' />
 
             </BarChart>
+            </ResponsiveContainer>
+            </div>
         </div>
     )
 }
