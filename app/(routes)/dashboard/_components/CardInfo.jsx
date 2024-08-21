803a5f68ca79605e2157952c1565104548f4a7dc
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 15:31:33 +0800

    Added conditional rendering to CardInfo component with fallback to animated skeleton cards when budgetList is empty

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -24,8 +24,10 @@ function CardInfo({ budgetList }) {
     console.log(totalBudget_,totalSpend_)
   }
   return (
+    <div>
+    {budgetList?.length>0? 
     <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-      <div className='p-7 border rounded-lg flex items-center justify-between'>
+    <div className='p-7 border rounded-lg flex items-center justify-between'>
         <div>
           <h2 className='text-sm text-slate-300'>Total Budgets</h2>
           <h2 className='font-bold text-2xl text-blue-600'>₱{totalBudget}</h2>
@@ -49,6 +51,17 @@ function CardInfo({ budgetList }) {
         <Wallet2
           className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
       </div>
+      </div>
+    :
+    <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      {[1,2,3].map((item,index)=>(
+        <div className='h-[110px] w-full bg-slate-900 animate-pulse rounded-lg'>
+
+        </div>
+      ))}
+    </div>
+    }
+
     </div>
   )
 }
