c5b5cca20436acac72c8687be5ddc79e09962951
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 13:30:12 +0800

    Updated CardInfo component: imported additional icons, changed grid layout, and replaced icons in cards.

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -1,9 +1,9 @@
-import { PiggyBankIcon } from 'lucide-react'
+import { PiggyBankIcon, ReceiptCentIcon, ReceiptIcon, Wallet2 } from 'lucide-react'
 import React from 'react'
 
 function CardInfo({ budgetList }) {
   return (
-    <div className='mt-7 grid grid-cols-1 md:grid-cols-3 gap-5'>
+    <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
       <div className='p-7 border rounded-lg flex items-center justify-between'>
         <div>
           <h2 className='text-sm text-slate-300'>Total Budgets</h2>
@@ -14,18 +14,18 @@ function CardInfo({ budgetList }) {
       </div>
       <div className='p-7 border rounded-lg flex items-center justify-between'>
         <div>
-          <h2 className='text-sm text-slate-300'>Total Budgets</h2>
+          <h2 className='text-sm text-slate-300'>Total Spend</h2>
           <h2 className='font-bold text-2xl text-blue-600'>₱15000</h2>
         </div>
-        <PiggyBankIcon
+        <ReceiptIcon
           className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
       </div>
       <div className='p-7 border rounded-lg flex items-center justify-between'>
         <div>
-          <h2 className='text-sm text-slate-300'>Total Budgets</h2>
+          <h2 className='text-sm text-slate-300'>No. of Budgets</h2>
           <h2 className='font-bold text-2xl text-blue-600'>₱15000</h2>
         </div>
-        <PiggyBankIcon
+        <Wallet2
           className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
       </div>
     </div>
