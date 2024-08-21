b69d63ba973ae1deecd42d09676141057df52e5f
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 16:08:43 +0800

    "Updated BarChartDashboard and CardInfo components with new implementations"

--- app/(routes)/dashboard/_components/BarChartDashboard.jsx
+++ app/(routes)/dashboard/_components/BarChartDashboard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BarChart, XAxis } from 'recharts'
 
 function BarChartDashboard({budgetList}) {
   return (
-    <div>BarChartDashboard</div>
+    <div>
+        <BarChart 
+        width={500}
+        height={300}
+        data={budgetList}
+        margin={{
+            top:5,
+            right:5,
+            left:5,
+            bottom:5
+        }}
+        >
+            <XAxis />
+
+        </BarChart>
+    </div>
   )
 }
 
--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -3,67 +3,67 @@ import React, { useEffect, useState } from 'react'
 
 function CardInfo({ budgetList }) {
 
-  const [totalBudget,setTotalBudget]=useState(0);
-  const [totalSpend,setTotalSpend]=useState(0);
-  
-  useEffect(()=>{
-    budgetList&&CalculateCardInfo();
-  },[budgetList])
-  const CalculateCardInfo=()=>{
-    console.log(budgetList);
-    let totalBudget_=0;
-    let totalSpend_=0;
+  const [totalBudget, setTotalBudget] = useState(0);
+  const [totalSpend, setTotalSpend] = useState(0);
 
-    budgetList.forEach(element =>{
-      totalBudget_=totalBudget_+Number(element.amount)
-      totalSpend_=totalSpend_+element.totalSpend
+  useEffect(() => {
+    budgetList && CalculateCardInfo();
+  }, [budgetList])
+  const CalculateCardInfo = () => {
+    console.log(budgetList);
+    let totalBudget_ = 0;
+    let totalSpend_ = 0;
+
+    budgetList.forEach(element => {
+      totalBudget_ = totalBudget_ + Number(element.amount)
+      totalSpend_ = totalSpend_ + element.totalSpend
     });
 
     setTotalBudget(totalBudget_);
     setTotalSpend(totalSpend_);
-    console.log(totalBudget_,totalSpend_)
+    console.log(totalBudget_, totalSpend_)
   }
   return (
     <div>
-    {budgetList?.length>0? 
-    <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-    <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
-    <PiggyBankIcon
-          className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
-        <div>
-          <h2 className='text-md text-white font-bold'>Total Budget</h2>
-          <h2 className='font-bold text-2xl text-blue-600'>₱{totalBudget}</h2>
-        </div>
-        
-      </div>
-      <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
-      <ReceiptIcon
-          className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
-       <div>
-          <h2 className='text-md text-white font-bold'>Total Spend</h2>
-          <h2 className='font-bold text-2xl text-blue-600'>₱{totalSpend}</h2>
-        </div>
-        
-      </div>
-      <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
-      <Wallet2
-          className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
-        <div>
-          <h2 className='text-md text-white font-bold'>Budget Count</h2>
-          <h2 className='font-bold text-2xl text-blue-600'>{budgetList?.length}</h2>
-        </div>
-        
-      </div>
-      </div>
-    :
-    <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-      {[1,2,3].map((item,index)=>(
-        <div className='h-[110px] w-full bg-slate-900 animate-pulse rounded-lg'>
+      {budgetList?.length > 0 ?
+        <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
+            <PiggyBankIcon
+              className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
+            <div>
+              <h2 className='text-md text-white font-bold'>Total Budget</h2>
+              <h2 className='font-bold text-2xl text-blue-600'>₱{totalBudget}</h2>
+            </div>
 
+          </div>
+          <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
+            <ReceiptIcon
+              className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
+            <div>
+              <h2 className='text-md text-white font-bold'>Total Spend</h2>
+              <h2 className='font-bold text-2xl text-blue-600'>₱{totalSpend}</h2>
+            </div>
+
+          </div>
+          <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
+            <Wallet2
+              className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
+            <div>
+              <h2 className='text-md text-white font-bold'>Budget Count</h2>
+              <h2 className='font-bold text-2xl text-blue-600'>{budgetList?.length}</h2>
+            </div>
+
+          </div>
         </div>
-      ))}
-    </div>
-    }
+        :
+        <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          {[1, 2, 3].map((item, index) => (
+            <div className='h-[110px] w-full bg-slate-900 animate-pulse rounded-lg'>
+
+            </div>
+          ))}
+        </div>
+      }
 
     </div>
   )
