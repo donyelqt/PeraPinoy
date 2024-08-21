ba160281e540d2a2451e1640fd58883d11e6e93f
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 13:15:25 +0800

    Added new HTML elements to CardInfo component

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function CardInfo({budgetList}) {
+function CardInfo({ budgetList }) {
   return (
     <div>
-
+      <div>
+        <div>
+          <h2 className='text-sm text-secondary'>Total Budgets</h2>
+          <h2 className='font-bold text-2xl text-white'>₱15000</h2>
+        </div>
+      </div>
     </div>
   )
 }
