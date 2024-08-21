8dbf75504bd7c1d0b6c4cb0b4f4fe21c880792e9
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 16:25:41 +0800

    "Updated BarChartDashboard component: added CSS class to container div, removed margin prop from BarChart"

--- app/(routes)/dashboard/_components/BarChartDashboard.jsx
+++ app/(routes)/dashboard/_components/BarChartDashboard.jsx
@@ -1,30 +1,24 @@
 import React from 'react'
 import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts'
 
-function BarChartDashboard({budgetList}) {
-  return (
-    <div>
-        <BarChart 
-        width={500}
-        height={300}
-        data={budgetList}
-        margin={{
-            top:5,
-            right:5,
-            left:5,
-            bottom:5
-        }}
-        >
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="totalSpend" stackId="a" fill='#4169e1 ' />
-            <Bar dataKey="amount" stackId="a" fill='#87cefa' />
+function BarChartDashboard({ budgetList }) {
+    return (
+        <div className='border rounded-lg p-10'>
+            <BarChart
+                width={500}
+                height={300}
+                data={budgetList}
+            >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="totalSpend" stackId="a" fill='#4169e1 ' />
+                <Bar dataKey="amount" stackId="a" fill='#87cefa' />
 
-        </BarChart>
-    </div>
-  )
+            </BarChart>
+        </div>
+    )
 }
 
 export default BarChartDashboard
