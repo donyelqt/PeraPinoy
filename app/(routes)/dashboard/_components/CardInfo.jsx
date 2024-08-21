ed1739bf9acd7bcd9ae8748ee1fea55395606979
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 13:24:54 +0800

    Added two new card info components with identical content, wrapped in a grid container

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -3,14 +3,30 @@ import React from 'react'
 
 function CardInfo({ budgetList }) {
   return (
-    <div className='mt-7'>
+    <div className='mt-7 grid grid-cols-1 md:grid-cols-3 gap-5'>
       <div className='p-7 border rounded-lg flex items-center justify-between'>
         <div>
           <h2 className='text-sm text-slate-300'>Total Budgets</h2>
           <h2 className='font-bold text-2xl text-blue-600'>₱15000</h2>
         </div>
-        <PiggyBankIcon 
-        className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
+        <PiggyBankIcon
+          className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
+      </div>
+      <div className='p-7 border rounded-lg flex items-center justify-between'>
+        <div>
+          <h2 className='text-sm text-slate-300'>Total Budgets</h2>
+          <h2 className='font-bold text-2xl text-blue-600'>₱15000</h2>
+        </div>
+        <PiggyBankIcon
+          className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
+      </div>
+      <div className='p-7 border rounded-lg flex items-center justify-between'>
+        <div>
+          <h2 className='text-sm text-slate-300'>Total Budgets</h2>
+          <h2 className='font-bold text-2xl text-blue-600'>₱15000</h2>
+        </div>
+        <PiggyBankIcon
+          className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
       </div>
     </div>
   )
