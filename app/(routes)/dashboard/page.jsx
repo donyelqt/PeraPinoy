6660aa41b60a82fac30f5ef681bf30de95ba1338
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-21 16:57:35 +0800

    "Formatted code in Dashboard page component, added whitespace and consistent indentation"

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -9,49 +9,50 @@ import BarChartDashboard from './_components/BarChartDashboard'
 import BudgetItem from './budgets/_components/BudgetItem';
 
 function Dashboard() {
-  const {user}=useUser();
+  const { user } = useUser();
 
-  const [budgetList,setBudgetList]=useState([]);
+  const [budgetList, setBudgetList] = useState([]);
 
-  useEffect(()=>{
-    user&&getBudgetList();
-  },[user])
+  useEffect(() => {
+    user && getBudgetList();
+  }, [user])
 
   // used to get budget list
-  const getBudgetList=async()=>{
+  const getBudgetList = async () => {
 
-    const result=await db.select({
-       ...getTableColumns(Budgets),
-       
-       totalSpend:sql `sum(${Expenses.amount}::numeric)`.mapWith(Number),
-       totalItem:sql `count(${Expenses.id})`.mapWith(Number)
+    const result = await db.select({
+      ...getTableColumns(Budgets),
+
+      totalSpend: sql`sum(${Expenses.amount}::numeric)`.mapWith(Number),
+      totalItem: sql`count(${Expenses.id})`.mapWith(Number)
     }).from(Budgets)
-    .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
-    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
-    .groupBy(Budgets.id)
-    .orderBy(desc(Budgets.id))
-    ;
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id))
+      ;
 
     setBudgetList(result);
 
   }
   return (
     <div className='p-10'>
-        <h2 className='font-bold text-tertiary text-4xl'><span className='text-blue-600'>Hello, </span>{user?.fullName}!👋</h2>
-        <p className='text-gray-500'>Empower Your Finances with PeraPinoy!</p>
-        <CardInfo budgetList={budgetList} />
-        <div className='grid grid-cols-1 md:grid-cols-3 mt-6 gap-5'>
-          <div className='md:col-span-2 text-blue-600'>
-            <BarChartDashboard
-            budgetList={budgetList} 
-            />
-          </div>
-          <div className='text-blue-600 grid gap-5'>
-            {budgetList.map((budget,index)=>(
-              <BudgetItem budget={budget} key={index} />
-            ))}
-          </div>
+      <h2 className='font-bold text-tertiary text-4xl'><span className='text-blue-600'>Hello, </span>{user?.fullName}!👋</h2>
+      <p className='text-gray-500'>Empower Your Finances with PeraPinoy!</p>
+      <CardInfo budgetList={budgetList} />
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-6 gap-5'>
+        <div className='text-blue-600 grid gap-5'>
+          <h2 className='text-tertiary font-bold text-4xl'>Recent Budgets</h2>
+          {budgetList.map((budget, index) => (
+            <BudgetItem budget={budget} key={index} />
+          ))}
         </div>
+        <div className='md:col-span-2 text-blue-600'>
+          <BarChartDashboard
+            budgetList={budgetList}
+          />
+        </div>
+      </div>
     </div>
   )
 }
