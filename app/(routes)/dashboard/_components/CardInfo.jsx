3430db6bd0e6e94c5b8d065c1ee7e727083eef85
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-23 12:01:22 +0800

    fixed bugs in clerk auth

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -29,6 +29,7 @@ function CardInfo({ budgetList }) {
         <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
           <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
             <PiggyBankIcon
+            key="icon"
               className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
             <div>
               <h2 className='text-md text-white font-bold'>Total Budget</h2>
@@ -38,6 +39,7 @@ function CardInfo({ budgetList }) {
           </div>
           <div className='p-7 border rounded-lg flex items-center justify-between hover:bg-slate-900'>
             <ReceiptIcon
+            key="icon"
               className='bg-tertiary p-3 h-12 w-12 rounded-lg text-white' />
             <div>
               <h2 className='text-md text-white font-bold'>Total Spend</h2>
