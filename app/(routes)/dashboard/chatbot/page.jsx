e3cde9dfe48609a3b77f30cbe1daf1e69c7c42f9
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-23 15:45:04 +0800

    "Updated padding of PeraPinoyGPT title from p-8 to p-4"

--- app/(routes)/dashboard/chatbot/page.jsx
+++ app/(routes)/dashboard/chatbot/page.jsx
@@ -5,7 +5,7 @@ import { Chatbot } from "./component/chatbot";
 function PeraPinoyGptPage() {
   return (
     <section>
-    <div className='font-bold text-tertiary text-6xl p-8'>PeraPinoyGPT</div>
+    <div className='font-bold text-tertiary text-6xl p-4'>PeraPinoyGPT</div>
     <div>
         <Chatbot />
     </div>
