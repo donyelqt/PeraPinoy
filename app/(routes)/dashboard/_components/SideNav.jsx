0f5912ea9d258516c89fa1b45ae5546742c8751a
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-08-24 13:43:59 +0800

    "Updated text in SideNav component from 'Profile' to 'User Profile'"

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -76,7 +76,7 @@ function SideNav() {
           <div className='fixed bottom-10 p-5 flex gap-2
           items-center'>
             <UserButton />
-            Profile
+            User Profile
           </div>
     </div>
   )
