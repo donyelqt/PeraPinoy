2461b5c2d018fe731db034ebdc81a33830acc20a
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-09-03 10:41:13 +0800

    "Added Image component to PremiumPage, replaced text with flex container and added membercard image"

--- app/(routes)/dashboard/premium/page.jsx
+++ app/(routes)/dashboard/premium/page.jsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
+import { membercard } from '../../../../public'
 
 function PremiumPage() {
   return (
-    <div className='font-bold text-tertiary text-5xl md:text-6xl lg:text-6xl p-10'>Premium</div>
+    <div className='text-tertiary p-10'>
+      <div className='flex items-center'>
+        <div className='font-bold text-5xl md:text-6xl lg:text-6xl'>Premium</div>
+        <Image className="w-10 h-10 object-contain ml-4"
+          src={membercard}
+          alt="membercard" />
+      </div>
+    </div>
   )
 }
 
