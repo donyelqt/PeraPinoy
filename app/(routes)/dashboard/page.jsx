9075b7aab7e031f70b9382452b7262cc02fb81e5
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-09-04 21:40:26 +0800

    "Update color of 'Hello' text from blue-600 to red-800 in dashboard page"

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -57,7 +57,7 @@ function Dashboard() {
   return (
     <div className="p-10 space-y-8">
       <div className="bg-blue-950 text-white p-7 rounded-3xl shadow-lg">
-        <h2 className='font-bold text-tertiary text-2xl md:text-4xl lg:text-4xl'><span className='text-blue-600'>Hello, </span>{user?.fullName}!👋</h2>
+        <h2 className='font-bold text-tertiary text-2xl md:text-4xl lg:text-4xl'><span className='text-red-800'>Hello, </span>{user?.fullName}!👋</h2>
         <p className='text-gray-500'>Empower Your Finances with PeraPinoy!</p>
       </div>
       <CardInfo budgetList={budgetList} />
