62147f5e2a38c9c2c812f9244d63e354a4cd5ca5
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-09-20 22:24:50 +0800

    "Updated PremiumPage component: changed 'flex items-center' to 'flex justify-center items-center' and no other changes."

--- app/(routes)/dashboard/premium/page.jsx
+++ app/(routes)/dashboard/premium/page.jsx
@@ -5,7 +5,7 @@ import { membercard } from '../../../../public'
 function PremiumPage() {
   return (
     <div className='text-tertiary p-10'>
-      <div className='flex items-center'>
+      <div className='flex justify-center items-center'>
         <div className='font-bold text-5xl md:text-6xl lg:text-6xl'>Premium</div>
         <Image className="w-10 h-10 object-contain ml-4"
           src={membercard}
