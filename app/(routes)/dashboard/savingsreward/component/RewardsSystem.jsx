2089e33a11a8b59654b62e046bc31281c959248b
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-10-10 20:34:18 +0800

    "Added a new button 'View Leaderboards' to the RewardsSystem component"

--- app/(routes)/dashboard/savingsreward/component/RewardsSystem.jsx
+++ app/(routes)/dashboard/savingsreward/component/RewardsSystem.jsx
@@ -6,6 +6,7 @@ const RewardsSystem = () => {
         <p className="text-gray-500">Meet Savings: <span className="font-bold text-green-600">5,000</span></p>
         <p className="text-gray-500">You need <span className="font-bold text-red-600">4,000</span> more savings for your next reward!</p>
         <button className="mt-4 bg-orange text-white py-2 px-4 rounded-3xl hover:bg-secondary">View Rewards</button>
+        <button className="mt-4 flex justify-center bg-secondary text-white py-2 px-4 rounded-3xl hover:bg-red-7000">View Leaderboards</button>
         
         {/* Add the progress bar here */}
         <div className="bg-red-500 rounded-full h-2.5 w-full mt-4">
