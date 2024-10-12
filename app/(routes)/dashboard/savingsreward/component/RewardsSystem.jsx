a342af6124ffe9b8c00fc20067015204d38ec13a
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-10-12 13:57:54 +0800

    "Updated button text in RewardsSystem component"

--- app/(routes)/dashboard/savingsreward/component/RewardsSystem.jsx
+++ app/(routes)/dashboard/savingsreward/component/RewardsSystem.jsx
@@ -5,8 +5,8 @@ const RewardsSystem = () => {
         <p className="text-gray-500">Current Savings: <span className="font-bold text-green-600">1,000</span></p>
         <p className="text-gray-500">Meet Savings: <span className="font-bold text-green-600">5,000</span></p>
         <p className="text-gray-500">You need <span className="font-bold text-red-600">4,000</span> more savings for your next reward!</p>
-        <button className="mt-4 bg-orange text-white py-2 px-4 rounded-3xl hover:bg-secondary">Rewards</button>
-        <button className="mt-4 flex justify-center bg-orange text-white py-2 px-4 rounded-3xl hover:bg-secondary">Leaderboards</button>
+        <button className="mt-4 bg-orange text-white py-2 px-4 rounded-3xl hover:bg-secondary">View Rewards</button>
+        <button className="mt-4 flex justify-center bg-orange text-white py-2 px-4 rounded-3xl hover:bg-secondary">View Leaderboards</button>
         
         {/* Add the progress bar here */}
         <div className="bg-red-500 rounded-full h-2.5 w-full mt-4">
