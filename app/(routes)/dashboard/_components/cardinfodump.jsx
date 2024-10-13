9ea235e431dc29b98198be3be556a7225b899752
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-10-13 22:09:21 +0800

    "Refactor CardInfo component to display budget information with icons and updated styling"

---
+++ app/(routes)/dashboard/_components/cardinfodump.jsx
@@ -0,0 +1,7 @@
+<div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+    {[1, 2, 3].map((item, index) => (
+        <div className='h-[140px] w-full bg-slate-900 animate-pulse rounded-3xl'>
+
+        </div>
+    ))}
+</div>
